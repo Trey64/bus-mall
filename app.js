97a5d866bdc230328895f44f36df6a5224b813cc
Trey64 <thomasellison.r@gmail.com>
2017-07-24 16:19:18 -0700

    added removeEventListener

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 function Image(name) {
   this.name = name;
   this.source = 'img/' + this.name + '.jpg';
@@ -9,27 +10,24 @@ function Image(name) {
 }
 
 Image.all = [];
+Image.selectionCounter = [];
 Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 for(var i = 0; i < Image.allNames.length; i++) {
   new Image(Image.allNames[i]);
 }
 
+
 Image.imgEl = document.getElementById('image1');
 Image.imgEl2 = document.getElementById('image2');
 Image.imgEl3 = document.getElementById('image3');
 
 function randomImage() {
-  // console.log(e.target.alt);
-  // if(e.target.alt === 'nine') {
-  //   alert('OMG A BABY GOAT');
-  // }
   var randomIndex = Math.floor(Math.random() * Image.all.length);
-  var randomIndex2 = Math.floor(Math.random() * Image.all.length);
-  var randomIndex3 = Math.floor(Math.random() * Image.all.length);
   Image.imgEl.src = Image.all[randomIndex].source;
   Image.imgEl.alt = Image.all[randomIndex].name;
   Image.all[randomIndex].timesShown += 1;
+  Image.selectionCounter.push('');
   console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + ' times.');
 }
 
@@ -63,3 +61,15 @@ document.getElementById('image3').addEventListener('click', randomImage3);
 randomImage();
 randomImage2();
 randomImage3();
+
+if (Image.selectionCounter.length === 25) {
+  document.getElementById('image1').removeEventListener('click', randomImage);
+  document.getElementById('image1').removeEventListener('click', randomImage2);
+  document.getElementById('image1').removeEventListener('click', randomImage3);
+  document.getElementById('image2').removeEventListener('click', randomImage);
+  document.getElementById('image2').removeEventListener('click', randomImage2);
+  document.getElementById('image2').removeEventListener('click', randomImage3);
+  document.getElementById('image3').removeEventListener('click', randomImage);
+  document.getElementById('image3').removeEventListener('click', randomImage2);
+  document.getElementById('image3').removeEventListener('click', randomImage3);
+}
